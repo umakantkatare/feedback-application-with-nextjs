dfd565bd243e09dd9f0e07b789dd864a4149ba0b
umakant katare <umakantkatare11@gmail.com>
2026-08-18 21:20:57 +0530

    add send messages route

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,62 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, content } = await request.json();
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found.",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting messages.",
+        },
+        {
+          status: 403,
+        },
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+
+    user.messages.push(newMessage as Message);
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message sent successfully.",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.error("Failed to send message:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "An unexpected error occurred while sending the message.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
